e1ea5601955c46ae3f575c3a1fe2b666b96cece0
Piero <pgarciam@gmail.com>
2024-06-22 18:14:32 -0500

    Automatización del ejercicio 2 (final).

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 #include <conio.h>
+#include <string>
 using namespace std; 
 int main(){
     int n1, n2;
     int i = 1, j = 0;
+    string get ,fn = "exit";
+do
+{
     cout << "Ingrese 2 valores: "<< endl; cin >> n1; cin >> n2;
+
     while (i <= n1 || i <= n2)
     {
         if (n1 % i == 0 && n2 % i == 0)
@@ -15,11 +20,14 @@ int main(){
     }
     if (j == 1)
     {
-        cout << "Son PESI."; 
+        cout << "Son PESI." << endl; 
     }
     else
     {
-        cout << "No son PESI.";
+        cout << "No son PESI." << endl;
     }
+    cout << "Si desea finalizar el programa escriba 'exit':" << endl;
+    cin >> get;
+} while (fn.compare(get) == -1);
     return EXIT_SUCCESS;
 }
